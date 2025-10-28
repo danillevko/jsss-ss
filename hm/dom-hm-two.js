2d9724330088afc2552ea65db7a12781a84a42c0
danillevko <danillevko1212@gmail.com>
2025-10-28 15:54:23 -0600

    hm

---
+++ hm/dom-hm-two.js
@@ -0,0 +1,30 @@
+// 1
+
+// const a = document.querySelector('#categories');
+// const b = a.querySelectorAll('.item');
+// console.log(b.length);
+
+// b.forEach(element => {
+//     const title = element.querySelector('h2').textContent;
+//     const number = element.querySelectorAll('ul li').length;
+//     console.log(title);
+//     console.log(number);
+// });
+
+// 2
+
+// const ingredients = [
+//  'Картопля',
+//  'Гриби',
+//  'Часник',
+//  'Помідори',
+//  'Зелень',
+//  'Приправи',
+// ];
+// const b = document.querySelector('#ingredients');
+// const a = ingredients.map(element => {
+//     const li = document.createElement('li');
+//     li.textContent = element;
+//     return li;
+// })
+// b.append(a);
