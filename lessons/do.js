a91b40bec4178cac2077379a8ce28d25cc9fee08
danillevko <danillevko1212@gmail.com>
2025-10-30 16:06:10 -0600

    do leesson

---
+++ lessons/do.js
@@ -0,0 +1,19 @@
+// let btn = document.querySelector('.btn');
+// btn.addEventListener('click', ()=>{
+//     btn.style.background = 'red';
+// })
+
+// let h1 = document.querySelector('h1')
+// let btn = document.querySelector('.btn');
+// btn.addEventListener('click', ()=>{
+//     h1.classList = 'header';
+// })btn.addEventListener('click', ()=>{
+//     h1.classList = 'header';
+// })
+
+let from = document.querySelector('.form');
+from.addEventListener('submit', ()=>{
+    alert('Ви зареєструвалися успішно! Дякую!');
+});
+let btn = document.querySelector('.btn');
+
