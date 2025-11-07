d86e2c96e4a3b3906b6d71f34a223486c024b104
danillevko <danillevko1212@gmail.com>
2025-11-07 15:32:39 -0700

    do hm

--- lessons/do.js
+++ lessons/do.js
@@ -11,9 +11,54 @@
 //     h1.classList = 'header';
 // })
 
-let from = document.querySelector('.form');
-from.addEventListener('submit', ()=>{
-    alert('Ви зареєструвалися успішно! Дякую!');
-});
-let btn = document.querySelector('.btn');
+// let from = document.querySelector('.form');
+// from.addEventListener('submit', ()=>{
+//     alert('Ви зареєструвалися успішно! Дякую!');
+// });
+// let btn = document.querySelector('.btn');
+
+
+// let form = document.querySelector('.form');
+// let input = document.querySelector('.input');
+// let btn = document.querySelector('.btn');
+// let p = document.querySelector('.p');
+// btn.addEventListener('click', (event)=>{
+//     event.preventDefault();
+//     p.textContent = input.value;
+// });
+
+
+// let body = document.querySelector('body')
+
+// function rgb(){
+    //     let r = Math.floor(Math.random() * 256);
+    //     let g = Math.floor(Math.random() * 256);
+    //     let b = Math.floor(Math.random() * 256);
+    //     return `rgb(${r}, ${g}, ${b})`;
+    // }
+    // btn.addEventListener('click', ()=>{
+        //     let color = rgb();
+        //     document.body.style.backgroundColor = color;
+        // });
+        
+// let btn = document.querySelector('.btn');
+// let hide = document.querySelector('.hidden');
+// btn.addEventListener('click', ()=>{
+//     hide.classList.toggle('.hidden');
+//     const isHidden = hide.classList.contains('.hidden');
+//     if(isHidden){
+//         hide.classList.remove('hidden');
+//         btn.textContent = 'Hide'
+//     } else{
+//         hide.classList.add('hidden');
+//         btn.textContent = 'Show up'
+//     }
+// });
+
+// let namee = prompt('what is ur name?');
+// alert(`hello Mr. ${namee} `)
+
+let btnUp = document.querySelector('.btn-plus');
+let btnDown = document.querySelector('.btn-minus');
+let text = document.querySelector('.text');
 
