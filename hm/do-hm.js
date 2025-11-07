61f6e76c4fb2b8c6a7ecc1080516cac0c869d797
danillevko <danillevko1212@gmail.com>
2025-11-07 15:34:27 -0700

    do hm remake

--- hm/do-hm.js
+++ hm/do-hm.js
@@ -1,22 +1,22 @@
-// const colors = document.querySelectorAll('input[name="color"]');
-// colors.forEach(btn =>{
-//     btn.addEventListener('click', ()=>{
-//         document.body.style.backgroundColor = btn.value;
-//     })
-// })
+const colors = document.querySelectorAll('input[name="color"]');
+colors.forEach(btn =>{
+    btn.addEventListener('click', ()=>{
+        document.body.style.backgroundColor = btn.value;
+    })
+})
 
 
-// const input = document.querySelector('#name-input');
-// const span = document.querySelector('#name-output');
-// const btn = document.querySelector('.transfer');
-// btn.addEventListener('click', (event)=>{
-//     event.preventDefault
-//     span.textContent = input.value;
-// })
+const input = document.querySelector('#name-input');
+const span = document.querySelector('#name-output');
+const btn = document.querySelector('.transfer');
+btn.addEventListener('click', (event)=>{
+    event.preventDefault
+    span.textContent = input.value;
+})
 
 
-// const slider = document.querySelector('#font-size-control');
-// const text = document.querySelector('#text');
-// slider.addEventListener('input', ()=>{
+const slider = document.querySelector('#font-size-control');
+const text = document.querySelector('#text');
+slider.addEventListener('input', ()=>{
 
-// })
+})
